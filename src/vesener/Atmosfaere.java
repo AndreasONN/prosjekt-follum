115de819feb5f6ba52ac5efd3745045f9f2e127a
AndreasONN <46299333+AndreasONN@users.noreply.github.com>
2023-08-27 04:01:54 +0200

    Foerste versjon av vesener ferdig, mangler aa implementere Skog.

--- src/vesener/Atmosfaere.java
+++ src/vesener/Atmosfaere.java
@@ -1,5 +1,26 @@
 package vesener;
 
+// Funker som baade karbonsluk og kilde til karbon.
 public class Atmosfaere {
+    private double atmosfaeriskKarbon;
 
+    public Atmosfaere(double initiellAtmosfaeriskKarbon) {
+        this.atmosfaeriskKarbon = initiellAtmosfaeriskKarbon;
+    }
+
+    public void mottaKarbon(double mengdeKarbon) {
+        atmosfaeriskKarbon += mengdeKarbon;
+    }
+
+    public void giKarbon(double mengdeKarbon) throws Error {
+        // TODO: behandle denne feilmeldingen riktig i resten av simuleringen.
+        if ((atmosfaeriskKarbon - mengdeKarbon) < 0) {
+            throw new Error("Ikke nok karbon i atmosfaeren!");
+        }
+        atmosfaeriskKarbon -= mengdeKarbon;
+    }
+
+    public double mengdeAtmosfaeriskKarbon() {
+        return atmosfaeriskKarbon;
+    }
 }
